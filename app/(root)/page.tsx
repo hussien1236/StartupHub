15be17ffd5ec26954ecea4a490a221399fbfd2bc
unknown <hussienhamdan06@gmail.com>
2025-03-06 23:34:46 +0200

    fixed fetching bug

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,15 @@
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { SanityLive } from "@/sanity/lib/live";
+import { client } from "@/sanity/lib/client";
 
 export default async function Home({searchParams}:{
   searchParams: Promise<{query?:string}>
 }){
   const query = (await searchParams).query;
   const params = {search:query || null};
-  const {data: posts} = await sanityFetch({query:STARTUPS_QUERY, params});
+  const posts = await client.fetch(STARTUPS_QUERY, params);
  return (
   <>
     <section className="pink_container">
