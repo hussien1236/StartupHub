306ef301a8003c3c297f317dacd5e3e24c06884e
unknown <hussienhamdan06@gmail.com>
2025-03-07 16:55:55 +0200

    Improved Tailwind CSS responsiveness

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -24,7 +24,7 @@ const StartupCard = ({post}: {post: StartupTypeCard} ) => {
           </div>
           <div className='flex-between mt-5 gap-5'>
             <div className='flex-1'>
-               <Link href={`/users/${author?._id}`}>
+               <Link href={`/user/${author?._id}`}>
                   <p className='text-16-medium line-clamp-1'>{author?.name}</p>
                </Link>
                <Link href={`/startup/${_id}`}>
@@ -33,7 +33,7 @@ const StartupCard = ({post}: {post: StartupTypeCard} ) => {
                </h3>
                </Link>              
             </div>
-            <Link href={`/users/${author?._id}`}>
+            <Link href={`/user/${author?._id}`}>
               <Image src={author?.image || "/default-avatar.png"} alt="placeholder" 
               width={48} height={48} className="rounded-full"/>
             </Link>
